ef0be0c3b1095f8331efd917e7ee780b143f39af
miguevrgo <miguevrgo@gmail.com>
2024-09-19 16:11:42 +0200

    Remove duplicate

--- PDOO/Practica4/Ruby/Irrgarten/game_character.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Irrgarten
-  module GameCharacter
-    PLAYER  = :player
-    MONSTER = :monster
-  end
-end
--- PDOO/Practica4/Ruby/Irrgarten/orientation.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Irrgarten
-  module Orientation
-    HORIZONTAL = :horizontal
-    VERTICAL   = :vertical
-  end
-end
